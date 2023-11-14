128fba23c2ec686d9dcd9969ffda199e868339b2
jetonpeche <nicolas.np63@gmail.com>
2023-11-14 21:13:13 +0100

    ajout des cache policy custom pour Authorize

---
+++ Mon.Template.Custom/PolicyOutputCache/CachePolicyNormal.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.OutputCaching;
+
+namespace Mon.Template.Custom.PolicyOutputCache;
+
+public sealed class CachePolicyNormal: IOutputCachePolicy
+{
+    public string NomTag { get; init; }
+
+    public CachePolicyNormal(string _nomTag) => NomTag = _nomTag;
+
+    public ValueTask CacheRequestAsync(OutputCacheContext context, CancellationToken cancellationToken)
+    {
+        var attemptOutputCaching = AttemptOutputCaching(context);
+
+        context.Tags.Add(NomTag);
+
+        context.EnableOutputCaching = true;
+        context.AllowCacheLookup = attemptOutputCaching;
+        context.AllowCacheStorage = attemptOutputCaching;
+        context.AllowLocking = true;
+
+        // durée de vie du cache
+        context.ResponseExpirationTimeSpan = TimeSpan.FromMinutes(20);
+
+        return ValueTask.CompletedTask;
+    }
+
+    // n'est pas utiliser quand il y a un authorize
+    public ValueTask ServeFromCacheAsync(OutputCacheContext context, CancellationToken cancellationToken)
+    {
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask ServeResponseAsync(OutputCacheContext context, CancellationToken cancellationToken)
+    {
+        context.AllowCacheStorage = true;
+
+        return ValueTask.CompletedTask;
+    }
+
+    private static bool AttemptOutputCaching(OutputCacheContext context)
+    {
+        var request = context.HttpContext.Request;
+
+        // activer cache si la méthode HTTP est GET
+        return HttpMethods.IsGet(request.Method);
+    }
+}
