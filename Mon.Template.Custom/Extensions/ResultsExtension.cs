5a137a791429c65d12424dcce7d57bc34349a8aa
jetonpeche <nicolas.np63@gmail.com>
2023-11-18 15:49:26 +0100

    ajout extension pour le OK sans reflexion / package en 7.0.xx a cause pomelo

--- Mon.Template.Custom/Extensions/ResultsExtension.cs
+++ Mon.Template.Custom/Extensions/ResultsExtension.cs
@@ -2,6 +2,8 @@
 using FluentValidation.Results;
 #endif
 
+using System.Text.Json.Serialization;
+
 namespace Mon.Template.Custom.Extensions
 {
     public static class ResultsExtension
@@ -29,7 +31,19 @@ namespace Mon.Template.Custom.Extensions
         /// <returns>Liste des erreurs de validation</returns>
         public static IResult ErreurConnexionBdd(this IResultExtensions ext)
         {
-            return Results.Problem(detail: "Impossible de se connecter à la base de données", statusCode: (int)HttpStatusCode.ServiceUnavailable);
+            return Results.Problem(detail: "Impossible de se connecter à la base de données", statusCode: StatusCodes.Status503ServiceUnavailable);
+        }
+
+        /// <summary>
+        /// Produit un code HTTP 200 OK sans utiliser la refléxion pour la sérialisation
+        /// </summary>
+        /// <param name="ext"></param>
+        /// <param name="_retour">donnée à retourner</param>
+        /// <param name="_retourContext">Le context du param '_retour'</param>
+        /// <returns>Renvoie les donnée en code HTTP 200 OK</returns>
+        public static IResult OK(this IResultExtensions ext, object? _retour, JsonSerializerContext _retourContext)
+        {
+            return Results.Json(_retour, _retourContext, statusCode: StatusCodes.Status200OK);
         }
     }
 }
